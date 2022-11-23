9cef82fa9693b0d25e0b57f0d3f3044242eeac9b
Nicolas <nicolasheredia02@gmail.com>
2022-11-23 16:47:12 -0300

    cambios en header

--- src/components/Header/Social.jsx
+++ src/components/Header/Social.jsx
@@ -16,3 +16,4 @@ function Social() {
 }
 
 export default Social;
+                                                                                                                                                
